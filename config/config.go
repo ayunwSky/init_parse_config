ba94da7e1862e89b8aa058e5e368e0e22ec6332f
ayunwSky <ayunwsky@qq.com>
2023-03-06 10:46:21 -0300

    feat: first commit

--- config/config.go
+++ config/config.go
@@ -1,21 +1,10 @@
 package config
 
-import (
-	"fmt"
-
-	"github.com/spf13/viper"
-)
-
-// Config 是全局的配置变量
-var Config *viper.Viper
-
-func init() {
-	Config = viper.New()
-	Config.SetConfigType("yaml")
-	Config.SetConfigFile("./config.yaml")
-
-	if err := Config.ReadInConfig(); err != nil {
-		fmt.Printf("Failed to read config file: %v\n", err)
-		return
-	}
+// Config 结构体用于存储配置数据
+type Config struct {
+	Port      int
+	Database  string
+	RedisHost string
+	RedisPort int
+	RedisPwd  string
 }
